ff012d66705e81147967f2c588e22a99466e447f
Tomasz Gandor <tgandor@future-processing.com>
2017-03-27 11:43:12 +0200

    this commit should be squashed later

--- src/bindings/python/openalpr/openalpr.py
+++ src/bindings/python/openalpr/openalpr.py
@@ -13,6 +13,7 @@ else:
     unicode = str
     _PYTHON_3 = True
 
+
 def _convert_to_charp(string):
     # Prepares function input for use in c-functions as char*
     if type(string) == unicode:
@@ -22,6 +23,7 @@ def _convert_to_charp(string):
     else:
         raise TypeError("Expected unicode string values or ascii/bytes values. Got: %r" % type(string))
 
+
 def _convert_from_charp(charp):
     # Prepares char* output from c-functions into Python strings
     if _PYTHON_3 and type(charp) == bytes:
@@ -29,7 +31,8 @@ def _convert_from_charp(charp):
     else:
         return charp
 
-class Alpr():
+
+class Alpr:
     def __init__(self, country, config_file, runtime_dir):
         """
         Initializes an OpenALPR instance in memory.
@@ -43,7 +46,7 @@ class Alpr():
         config_file = _convert_to_charp(config_file)
         runtime_dir = _convert_to_charp(runtime_dir)
         try:
-        # Load the .dll for Windows and the .so for Unix-based
+            # Load the .dll for Windows and the .so for Unix-based
             if platform.system().lower().find("windows") != -1:
                 self._openalprpy_lib = ctypes.cdll.LoadLibrary("libopenalprpy.dll")
             elif platform.system().lower().find("darwin") != -1:
@@ -89,7 +92,6 @@ class Alpr():
 
         self._set_detect_region_func = self._openalprpy_lib.setDetectRegion
         self._set_detect_region_func.argtypes = [ctypes.c_void_p, ctypes.c_bool]
-
 
         self._set_top_n_func = self._openalprpy_lib.setTopN
         self._set_top_n_func.argtypes = [ctypes.c_void_p, ctypes.c_int]
@@ -202,7 +204,6 @@ class Alpr():
         prewarp = _convert_to_charp(prewarp)
         self._set_prewarp_func(self.alpr_pointer, prewarp)
 
-
     def set_default_region(self, region):
         """
         This sets the default region for detecting license plates. For example,
@@ -223,7 +224,6 @@ class Alpr():
         :return: None
         """
         self._set_detect_region_func(self.alpr_pointer, enabled)
-
 
     def __del__(self):
         if self.is_loaded():
